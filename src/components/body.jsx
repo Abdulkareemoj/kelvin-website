0724a41561f3af158ed159c806ccb6e5795eb8c9
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-11-24 15:53:26 +0100

    clear

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import { render } from 'react-dom';
 import { AiFillFacebook, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle, AiOutlineWhatsApp } from 'react-icons/ai';
 
