f793f1735b06d17b9e236c3f73ed39277885c3bd
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-11-24 15:49:45 +0100

    add lightbox

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,4 +1,8 @@
+import React, { Component } from 'react';
+// import { render } from 'react-dom';
 import { AiFillFacebook, AiFillInstagram, AiFillLinkedin, AiFillTwitterCircle, AiOutlineWhatsApp } from 'react-icons/ai';
+
+import Gallery from './SimpleGallery';
 
 export default function Hero() {
   return (
@@ -56,8 +60,39 @@ export default function Hero() {
         
           <div>
             
-            <div className="text-3xl py-1 px-8" />
-            
+            <div className="text-3xl py-1 px-8">
+            <div>
+        <Gallery
+          galleryID="my-test-gallery"
+          images={[
+            {
+              largeURL:
+                'https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-2500.jpg',
+              thumbnailURL:
+                'https://cdn.photoswipe.com/photoswipe-demo-images/photos/1/img-200.jpg',
+              width: 1875,
+              height: 2500,
+            },
+            {
+              largeURL:
+                'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg',
+              thumbnailURL:
+                'https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg',
+              width: 1669,
+              height: 2500,
+            },
+            {
+              largeURL:
+                'https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg',
+              thumbnailURL:
+                'https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg',
+              width: 2500,
+              height: 1666,
+            },
+          ]}
+        />
+      </div>
+            </div>
           </div>
 
         </section>
