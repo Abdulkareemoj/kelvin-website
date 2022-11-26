1699e2bb126320358d2a1acdaf3b9f92b6188e5a
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-11-26 19:17:27 +0100

    remove toggle

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Switcher from './darkModeToggle';
 
 
 export default function Navbar() {
@@ -12,7 +11,7 @@ export default function Navbar() {
                         Welcome
                     </a>
                                         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700   flex flex-wrap items-center text-base justify-center" />
-<Switcher />
+
                     <a
                         href="#contact"
                         className="inline-flex items-center  py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
