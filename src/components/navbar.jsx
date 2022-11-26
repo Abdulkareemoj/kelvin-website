9572d108742bbfe425876e1e73a55304fc9f0386
Abdulkareemoj <abdulkareemojerinde@gmail.com>
2022-11-26 19:09:38 +0100

    darkmode

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
+
 import Switcher from './darkModeToggle';
+
 
 export default function Navbar() {
     return (
--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 
 import pic from '../assets/images/profile.jpg';
 
